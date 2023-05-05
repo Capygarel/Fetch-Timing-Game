4c4f819f3f7183630ad630f52ceeb52137d0c034
Thoringar <guillaumedelboulbes@gmail.com>
2023-05-05 21:05:40 +0200

    Initial upload

---
+++ Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CompareTag("Dog"))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        else
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
